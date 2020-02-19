b4742d06a99c8455f5cd940bb8c11edc7dc76de7
javihu92 <jahuerta@inf.uc3m.es>
2020-02-19 16:10:12 +0100

    dev 0.1.2d: fixed player list

--- src/evento.py
+++ src/evento.py
@@ -238,7 +238,7 @@ ARGUMENT_TYPES = {
                       finder=re.compile(r'Director: ([^·]+) [·|\n]', re.IGNORECASE),
                       dtype=str),
     'Jugadores': Campo(alias='j', name='jugador', default=[],
-                       finder=re.compile(r'\n- *([^\n]+) *\n', re.IGNORECASE),
+                       finder=re.compile(r'\n- *([^\n]+) *', re.IGNORECASE),
                        dtype=str),
     'Maximo': Campo(alias='m', name='maximo', default=6,
                     finder=re.compile(r'Maximo: *(\d+) *[·|\n]', re.IGNORECASE),
