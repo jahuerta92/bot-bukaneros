647e29c087764861a42dbfee75f12f1bf86e4869
javihu92 <javihu92@gmail.com>
2020-02-22 11:04:02 +0100

    dev 0.2: added mover

--- bot.py
+++ bot.py
@@ -7,17 +7,19 @@ from src.evento import ARGUMENT_TYPES, LEGAL_DAYS_SET, Evento, Dia
 from discord.ext.commands import Bot
 from datetime import date
 
-
 BOT_PREFIX = "+"
-TOKEN = 'NTQwNTA5MDU3MTc0NDcwNjg2.Xkf4_g.fOur5nnRHLhBGUnTh1nfqSgmFGw'  # Get at discordapp.com/developers/applications/me
+BETA_TOKEN = 'NjgwNzA4NzUyNzQ3MjY2MDUy.XlD1eg.kviJenW2IPCYWpJN5j8OScZo4_U'
+DEPLOY_TOKEN = 'NTQwNTA5MDU3MTc0NDcwNjg2.Xkf4_g.fOur5nnRHLhBGUnTh1nfqSgmFGw'  # Get at discordapp.com/developers/applications/me
 TAG = '[EventoBukanero]'
 bot = Bot(command_prefix=BOT_PREFIX,
           description='Organiza las partidas de rol. Si no conoces los comandos usa ++ayuda')
 
-def simple_cmp(a,b):
+
+def simple_cmp(a, b):
     a_sub = unidecode.unidecode(a).lower()
     b_sub = unidecode.unidecode(b).lower()
     return a_sub == b_sub
+
 
 def parse(*args, optional_fields, required_fields):
     opt_fields = [('-' + ARGUMENT_TYPES[k].alias, '--' + ARGUMENT_TYPES[k].name) for k in optional_fields]
@@ -32,11 +34,11 @@ def parse(*args, optional_fields, required_fields):
         alias, name = field
         if alias not in arg_names and name not in arg_names:
             if field in req_fields:
-                return False, '*Error*: El argumento *{}* es requerido, pero falta en el comando.'.format(name)
+                return False, '**Error**: El argumento **{}** es requerido, pero falta en el comando.'.format(name)
 
     for item in arg_names:
         if item not in [item for field in all_fields for item in field]:
-            return False, '*Error*: El argumento *{}* no esta permitido para este comando.'.format(item)
+            return False, '**Error**: El argumento **{}** no esta permitido para este comando.'.format(item)
 
     parsed_dict = dict()
 
@@ -46,21 +48,21 @@ def parse(*args, optional_fields, required_fields):
         value = None
 
         if arg_alias in args and arg_name in args:
-            return False, '*Error*: Argumentos *{}* y *{}* están repetidos.'.format(arg_alias, arg_name)
+            return False, '**Error**: Argumentos **{}** y **{}** están repetidos.'.format(arg_alias, arg_name)
         elif arg_alias in args:
             if args.count(arg_alias) > 1:
-                return False, '*Error*: Argumento *{}* aparece por duplicado.'.format(arg_alias)
+                return False, '**Error**: Argumento **{}** aparece por duplicado.'.format(arg_alias)
             idx = args.index(arg_alias)
         elif arg_name in args:
             if args.count(arg_name) > 1:
-                return False, '*Error*: Argumento *{}* aparece por duplicado.'.format(arg_name)
+                return False, '**Error**: Argumento **{}** aparece por duplicado.'.format(arg_name)
             idx = args.index(arg_name)
 
         if arg_alias in args or arg_name in args:
             if idx >= len(args) - 1:
-                return False, '*Error*: Falta un valor para el argumento {}'.format(args[idx])
+                return False, '**Error**: Falta un valor para el argumento {}'.format(args[idx])
             elif args[idx + 1][0] == alias_prefix:
-                return False, '*Error*: Falta un valor para el argumento {}'.format(args[idx])
+                return False, '**Error**: Falta un valor para el argumento {}'.format(args[idx])
 
             value_list = []
             idx += 1
@@ -74,6 +76,82 @@ def parse(*args, optional_fields, required_fields):
         parsed_dict[key] = value
 
     return True, parsed_dict
+
+
+@bot.command(name='mover',
+            description='Mueve a un jugador de una partida a otra',
+            pass_context=True)
+async def mover(ctx, idnt1, idnt2, *args):
+    optional_fields = {'Jugadores'}
+    required_fields = set()
+    check, value = parse(*args, optional_fields=optional_fields, required_fields=required_fields)
+
+    if not check:
+        await ctx.send(value)
+        return False
+
+    if idnt1 is None:
+        await ctx.send('**Error**: Falta identificador de la primera partida, el primer argumento.')
+        return False
+    if idnt2 is None:
+        await ctx.send('**Error**: Falta identificador de la segunda partida, el segundo argumento.')
+        return False
+
+    author = ctx.message.author
+
+    if len(args) < 1:
+        value['Jugadores'] = author.nick if author.nick is not None else author.name
+
+    pinned = await ctx.pins()
+
+    try:
+        old_events = [Evento(message) for message in pinned if TAG in message.content]
+    except:
+        await ctx.send(
+            "**Error**: Ha ocurrido un error al intentar recuperar los eventos."
+            "Por favor revisa que los mensajes anclados tengan el formato correcto."
+            "Si usas **+plantilla** te mandaré un ejemplo de como tienen que estar escritos los eventos.")
+        return False
+
+    this_events_1 = [event for event in old_events if simple_cmp(event.event_dict['Id'], idnt1)]
+    this_events_2 = [event for event in old_events if simple_cmp(event.event_dict['Id'], idnt2)]
+
+    if len(this_events_1) < 1:
+        await ctx.send("**Error**: No existe la partida **{}** \n".format(idnt1))
+        return False
+    elif len(this_events_1) > 1:
+        await ctx.send(
+            "**Error**: Hay múltiples partidas de **{}**, por favor revisa los mensajes anclados. \n".format(idnt1))
+        return False
+    if len(this_events_2) < 1:
+        await ctx.send("**Error**: No existe la partida **{}** \n".format(idnt2))
+        return False
+    elif len(this_events_2) > 1:
+        await ctx.send(
+            "**Error**: Hay múltiples partidas de **{}**, por favor revisa los mensajes anclados. \n".format(idnt2))
+        return False
+
+    this_event_1 = this_events_1[0]
+    this_event_2 = this_events_2[0]
+
+    check, fail = this_event_1.remove_player(value['Jugadores'])
+    if not check:
+        await ctx.send(fail.format(value['Jugadores'], idnt1))
+        return False
+    check, fail = this_event_2.new_player(value['Jugadores'])
+    if not check:
+        await ctx.send(fail.format(value['Jugadores'], idnt2))
+        return False
+
+    new_message_1 = await ctx.send(str(this_event_1))
+    new_message_2 = await ctx.send(str(this_event_2))
+
+    await this_event_1.unpin()
+    await this_event_2.unpin()
+    await new_message_1.pin()
+    await new_message_2.pin()
+
+    return True
 
 @bot.command(name='apuntar',
              description="Apunta a un jugador a una partida",
@@ -89,8 +167,8 @@ async def apuntar(ctx, idnt=None, *args):
         return False
 
     if idnt is None:
-        await ctx.send('*Error*: Falta identificador de la partida, el primer argumento.')
-        return
+        await ctx.send('**Error**: Falta identificador de la partida, el primer argumento.')
+        return False
 
     author = ctx.message.author
 
@@ -103,17 +181,18 @@ async def apuntar(ctx, idnt=None, *args):
         old_events = [Evento(message) for message in pinned if TAG in message.content]
     except:
         await ctx.send(
-            "*Error*: Ha ocurrido un error al intentar recuperar los eventos."
+            "**Error**: Ha ocurrido un error al intentar recuperar los eventos."
             "Por favor revisa que los mensajes anclados tengan el formato correcto."
-            "Si usas *+plantilla* te mandaré un ejemplo de como tienen que estar escritos los eventos.")
+            "Si usas **+plantilla** te mandaré un ejemplo de como tienen que estar escritos los eventos.")
         return False
 
     this_events = [event for event in old_events if simple_cmp(event.event_dict['Id'], idnt)]
     if len(this_events) < 1:
-        await ctx.send("*Error*: No existe la partida *{}* \n".format(idnt))
+        await ctx.send("**Error**: No existe la partida **{}** \n".format(idnt))
         return False
     elif len(this_events) > 1:
-        await ctx.send("*Error*: Hay múltiples partidas de *{}*, por favor revisa los mensajes anclados. \n".format(idnt))
+        await ctx.send(
+            "**Error**: Hay múltiples partidas de **{}**, por favor revisa los mensajes anclados. \n".format(idnt))
         return False
 
     this_event = this_events[0]
@@ -142,31 +221,32 @@ async def quitar(ctx, idnt=None, *args):
         return False
 
     if idnt is None:
-        await ctx.send('*Error*: Falta identificador de la partida, el primer argumento.')
+        await ctx.send('**Error**: Falta identificador de la partida, el primer argumento.')
         return False
 
     author = ctx.message.author
 
     if len(args) < 1:
         value['Jugadores'] = author.nick if author.nick is not None else author.name
-    
+
     pinned = await ctx.pins()
 
     try:
         old_events = [Evento(message) for message in pinned if TAG in message.content]
     except:
         await ctx.send(
-            "*Error*: Ha ocurrido un error al intentar recuperar los eventos.\n"
+            "**Error**: Ha ocurrido un error al intentar recuperar los eventos.\n"
             "Por favor revisa que los mensajes anclados tengan el formato correcto.\n"
-            "Si usas *+plantilla* te mandaré un ejemplo de como tienen que estar escritos los eventos.")
+            "Si usas **+plantilla** te mandaré un ejemplo de como tienen que estar escritos los eventos.")
         return False
 
     this_events = [event for event in old_events if simple_cmp(event.event_dict['Id'], idnt)]
     if len(this_events) < 1:
-        await ctx.send("*Error*: No existe la partida *{}* \n".format(idnt))
+        await ctx.send("**Error**: No existe la partida **{}** \n".format(idnt))
         return False
     elif len(this_events) > 1:
-        await ctx.send("*Error*: Hay múltiples partidas de *{}*, por favor revisa los mensajes anclados. \n".format(idnt))
+        await ctx.send(
+            "**Error**: Hay múltiples partidas de **{}**, por favor revisa los mensajes anclados. \n".format(idnt))
         return False
 
     this_event = this_events[0]
@@ -194,7 +274,7 @@ async def crear(ctx, idnt=None, *args):
         return False
 
     if idnt is None:
-        await ctx.send('*Error*: Falta identificador de la partida, el primer argumento.')
+        await ctx.send('**Error**: Falta identificador de la partida, el primer argumento.')
         return False
 
     value['Id'] = idnt
@@ -205,12 +285,12 @@ async def crear(ctx, idnt=None, *args):
         tmp = channel.name.split('-')
         if len(tmp) < 2:
             ctx.send(
-                '*Error*: El dia no se puede establecer automaticamente en este canal. '
+                '**Error**: El dia no se puede establecer automaticamente en este canal. '
                 'Prueba a especificarlo con -d o --dia.')
             return False
         elif tmp[1] not in LEGAL_DAYS_SET:
             ctx.send(
-                '*Error*: El dia no se puede establecer automaticamente en este canal. '
+                '**Error**: El dia no se puede establecer automaticamente en este canal. '
                 'Prueba a especificarlo con -d o --dia.')
             return False
         value['Dia'] = Dia(tmp[1])
@@ -227,13 +307,13 @@ async def crear(ctx, idnt=None, *args):
         old_events = [Evento(message) for message in pinned if TAG in message.content]
     except:
         await ctx.send(
-            "*Error*: Ha ocurrido un error al intentar recuperar los eventos.\n"
+            "**Error**: Ha ocurrido un error al intentar recuperar los eventos.\n"
             "Por favor revisa que los mensajes anclados tengan el formato correcto.\n"
-            "Si usas *+plantilla* te mandaré un ejemplo de como tienen que estar escritos los eventos.")
+            "Si usas **+plantilla** te mandaré un ejemplo de como tienen que estar escritos los eventos.")
         return False
 
     if new_event in old_events:
-        await ctx.send("*Error*: Ya existe la partida *{}*".format(new_event.event_dict['Id']))
+        await ctx.send("**Error**: Ya existe la partida **{}**".format(new_event.event_dict['Id']))
         return False
 
     new_message = await ctx.send(new_event)
@@ -254,7 +334,7 @@ async def anular(ctx, idnt=None, *args):
         return False
 
     if idnt is None:
-        await ctx.send('*Error*: Falta identificador de la partida, el primer argumento.')
+        await ctx.send('**Error**: Falta identificador de la partida, el primer argumento.')
         return
 
     author = ctx.message.author
@@ -264,32 +344,35 @@ async def anular(ctx, idnt=None, *args):
     try:
         old_events = [Evento(message) for message in pinned if TAG in message.content]
     except:
-        await ctx.send("*Error*: Ha ocurrido un error al intentar recuperar los eventos.\n"
+        await ctx.send("**Error**: Ha ocurrido un error al intentar recuperar los eventos.\n"
                        "Por favor revisa que los mensajes anclados tengan el formato correcto.\n"
-                       "Si usas *+plantilla* te mandaré un ejemplo de como tienen que estar escritos los eventos.")
+                       "Si usas **+plantilla** te mandaré un ejemplo de como tienen que estar escritos los eventos.")
         return False
 
     this_events = [event for event in old_events if simple_cmp(event.event_dict['Id'], idnt)]
 
     if len(this_events) < 1:
-        await ctx.send("*Error*: No existe la partida *{}* \n".format(idnt))
+        await ctx.send("**Error**: No existe la partida **{}** \n".format(idnt))
         return False
     elif len(this_events) > 1:
-        await ctx.send("*Error*: Hay múltiples partidas de *{}*, por favor revisa los mensajes anclados. \n".format(idnt))
+        await ctx.send(
+            "**Error**: Hay múltiples partidas de **{}**, por favor revisa los mensajes anclados. \n".format(idnt))
         return False
 
     nick = author.nick if author.nick is not None else author.name
 
     this_event = this_events[0]
     if nick != this_event.event_dict['Director']:
-        await ctx.send("*Error*: No puedes anular una partida que no diriges")
+        await ctx.send("**Error**: No puedes anular una partida que no diriges")
         return False
 
     await ctx.send("Anulada partida " + idnt)
     await this_event.unpin()
     return True
 
+
 bot.remove_command('help')
+
 
 @bot.command(name='help',
              aliases=['ayuda', '?', 'ayudame'],
@@ -334,30 +417,31 @@ Bienvenido al organizador de partidas de Bukaneros. Soy el panel de ayuda de est
              aliases=['ejemplo'],
              pass_ctx=True)
 async def plantilla(ctx):
-    await ctx.message.author.send('Este es un ejemplo del formato de la partida. Copia y pega el contenido del mensaje para'
-                                  'que sea compatible con el bot.\n\n'
-                                  '[EventoBukanero] Partida de rol. Id: D&D · Nombre: La Maldición de Strahd ·'
-                                  ' Dia: Miércoles 12/02/2020 · Inicio: 15:30 · Fin: 19:30 · Director: Javi ·'
-                                  ' Maximo: 5 · Notas: Hola caracola \n'
-                                  '[Jugadores]\n'
-                                  '- Abel\n'
-                                  '- Bea\n'
-                                  '- Carlos\n'
-                                  '-')
+    await ctx.message.author.send(
+        'Este es un ejemplo del formato de la partida. Copia y pega el contenido del mensaje para'
+        'que sea compatible con el bot.\n\n'
+        '[EventoBukanero] Partida de rol. Id: D&D · Nombre: La Maldición de Strahd ·'
+        ' Dia: Miércoles 12/02/2020 · Inicio: 15:30 · Fin: 19:30 · Director: Javi ·'
+        ' Maximo: 5 · Notas: Hola caracola \n'
+        '[Jugadores]\n'
+        '- Abel\n'
+        '- Bea\n'
+        '- Carlos\n'
+        '-')
 
 
 @bot.command(name='listar',
              description="Devuelve un listado de partidas en todo el servidor",
-                pass_context=True)
+             pass_context=True)
 async def listar(ctx):
     try:
         event_list = [Evento(message) for channel in ctx.guild.text_channels
                       for message in await channel.pins()
                       if TAG in message.content]
     except:
-        await ctx.send("*Error*: Ha ocurrido un error al intentar recuperar los eventos."
+        await ctx.send("**Error**: Ha ocurrido un error al intentar recuperar los eventos."
                        "Por favor revisa que los mensajes anclados tengan el formato correcto."
-                       "Si usas *+plantilla* te mandaré un ejemplo de como tienen que estar escritos los eventos.")
+                       "Si usas **+plantilla** te mandaré un ejemplo de como tienen que estar escritos los eventos.")
         return False
 
     if len(event_list) < 1:
@@ -375,6 +459,7 @@ async def listar(ctx):
 
     await ctx.message.author.send(embed=embed)
     return True
+
 
 async def list_servers():
     await bot.wait_until_ready()
@@ -394,5 +479,6 @@ async def on_message(message):
 
     return True
 
+
 bot.loop.create_task(list_servers())
-bot.run(TOKEN)
+bot.run(BETA_TOKEN)
